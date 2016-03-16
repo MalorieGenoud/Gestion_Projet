ba89072144b865ec6081d8c51bf7fd04a99f25ba
aiido <aida.sejmenovic@cpnv.ch>
2016-03-16 14:53:38 +0100

    Tests laravel 2

---
+++ brouillon code/AidaLaravel/blog/app/Http/Controllers/PagesController.php
@@ -0,0 +1,23 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller {
+
+    public function about(){
+        $title = "A propos";
+        $numbers = [1,2,3,4,5,6,7,8,9];
+        return view('pages/about', compact('title', 'numbers'));
+
+    }
+
+    public function login(){
+        $title = "A propos";
+
+        return view('pages/login', compact('title', 'numbers'));
+
+    }
+}
---
+++ brouillon code/AidaLaravel/blog/resources/views/pages/about.blade.php
@@ -0,0 +1,29 @@
+@extends("default")
+@section('title', $title)
+
+@section('content')
+    <h1>{{ $title }}</h1>
+
+    <p>UIAUISDFIAGBUSDFGAU SGDFUVAShidagsudfgvasuvdfva</p>
+
+
+        <ul>
+            @forelse($numbers as $number)
+                <li>{{$number}}</li>
+            @empty
+                <li>Aucun chiffre</li>
+
+            @endforelse
+        </ul>
+
+
+@endsection
+
+
+@section('sidebar')
+
+    <h3>A propos</h3>
+    <p>UIAUISDFIAGBUSDFGAU SGDFUVAShidagsudfgvasuvdfva</p>
+
+    @endsection
+    
