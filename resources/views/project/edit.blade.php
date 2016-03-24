cb5652c89cf1a87e1e4a604d0bbf353c0ee96bde
aiido <aida.sejmenovic@cpnv.ch>
2016-03-24 14:59:27 +0100

    Update of the views, for better comprehension

--- resources/views/project/edit.blade.php
+++ resources/views/project/edit.blade.php
@@ -1,14 +1,14 @@
-@extends('layouts/app')
+@extends('layouts.app')
 
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Vos projets</div>
+                    <div class="panel-heading">Page d'édition</div>
 
                     <div class="panel-body">
-                        Vous êtes loggé ! Voilà vos projets !
+                        Modifiez votre projet ici
                     </div>
                 </div>
             </div>
--- resources/views/project/task.blade.php
+++ resources/views/project/task.blade.php
@@ -1,14 +1,14 @@
-@extends('layouts/app')
+@extends('layouts.app')
 
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Votre projet</div>
+                    <div class="panel-heading">Votre tâches</div>
 
                     <div class="panel-body">
-                        Votre projet :
+                        Votre liste de tâches
                     </div>
                 </div>
             </div>
